1fc9e8bf89934b97eb173e952c0c2caa0e2945f1
HeeSungLee <102288399+lheesung@users.noreply.github.com>
2023-10-31 12:20:04 +0900

    ADD: fonts

--- src/styles/font.ts
+++ src/styles/font.ts
@@ -1,25 +1,32 @@
-const fontGenerator = (
-  weight: number,
-  size: number,
-  lineHeight: number,
-  letterSpacing: number,
-) => `
+const Bold = 700;
+const SemiBold = 600;
+const Medium = 500;
+const Regular = 400;
+
+const fontGenerator = (weight: number, size: number, lineHeight: number) => `
       font-weight: ${weight};
       font-size: ${size}px;
       line-height: ${lineHeight}%;
-      letter-spacing: ${letterSpacing}px;
+      letter-spacing: 0px;
       `;
 
 export const font = {
-  $titleLarge: fontGenerator(700, 32, 150, 0),
-  $title01: fontGenerator(700, 26, 150, 0),
-  $title02: fontGenerator(700, 20, 150, 0),
-  $title03: fontGenerator(600, 18, 150, 0),
-  $title04: fontGenerator(500, 13, 150, 0),
-  $body: fontGenerator(500, 16, 150, 0),
-  $headline: fontGenerator(500, 15, 150, 0),
-  $callout: fontGenerator(400, 14, 150, 0),
-  $subhead: fontGenerator(400, 13, 150, 0),
-  $footnote: fontGenerator(400, 12, 150, 0),
-  $caption: fontGenerator(400, 11, 150, 0),
+  $D1: fontGenerator(Bold, 72, 120),
+  $D2: fontGenerator(Bold, 64, 120),
+  $D3: fontGenerator(Bold, 56, 120),
+  $H1: fontGenerator(SemiBold, 48, 150),
+  $H2: fontGenerator(SemiBold, 44, 150),
+  $H3: fontGenerator(SemiBold, 40, 150),
+  $H4: fontGenerator(SemiBold, 36, 150),
+  $H5: fontGenerator(SemiBold, 32, 150),
+  $Body1: fontGenerator(Medium, 28, 150),
+  $Body2: fontGenerator(Medium, 24, 150),
+  $Body3: fontGenerator(Medium, 20, 150),
+  $p1: fontGenerator(Regular, 16, 150),
+  $p2: fontGenerator(Regular, 14, 150),
+  $p3: fontGenerator(Regular, 12, 150),
+  $Button1: fontGenerator(Medium, 24, 120),
+  $Button2: fontGenerator(Medium, 20, 120),
+  $Button3: fontGenerator(Medium, 16, 120),
+  $Navigation: fontGenerator(Regular, 12, 130),
 };
