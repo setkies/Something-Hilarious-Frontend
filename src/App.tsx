0122ee46353a38948ba9dc689df16f3f36b3255a
이희성 <ihuiseong@ihuiseong-ui-noteubug.local>
2023-11-19 11:58:01 +0900

    ADD :: useModal 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,23 @@ import Landing from './pages/Landing';
 import SignUp from 'pages/SignUp';
 import Funding from 'pages/Fundings';
 import Detail from 'pages/Detail';
+import { RecoilRoot } from 'recoil';
+import Modal from 'components/Modal';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Landing />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/funding' element={<Funding />} />
-        <Route path='/detail/:id' element={<Detail />} />
-        <Route path='*' element={<>404 NOT FOUND</>} />
-      </Routes>
-    </BrowserRouter>
+    <RecoilRoot>
+      <Modal />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/funding' element={<Funding />} />
+          <Route path='/detail/:id' element={<Detail />} />
+          <Route path='*' element={<>404 NOT FOUND</>} />
+        </Routes>
+      </BrowserRouter>
+    </RecoilRoot>
   );
 };
 
