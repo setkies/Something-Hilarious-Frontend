c11178ecb899744b03639a3d6dc8119dd5b13dff
이희성 <ihuiseong@ihuiseong-ui-noteubug.local>
2023-12-10 21:25:01 +0900

    ADD :: toastify setting

--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,38 @@ import Modal from 'components/Modal';
 import MyPage from 'pages/MyPage';
 import Auth from './pages/auth';
 import AuthWrapper from 'components/AuthWrapper';
+import { ToastContainer } from 'react-toastify';
 
 const App = () => {
   return (
-    <RecoilRoot>
-      <AuthWrapper>
-        <Modal />
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Landing />} />
-            <Route path='/signup' element={<SignUp />} />
-            <Route path='/funding' element={<Funding />} />
-            <Route path='/detail/:id' element={<Detail />} />
-            <Route path='/mypage' element={<MyPage />} />
-            <Route path='/auth/google' element={<Auth />} />
-            <Route path='*' element={<>404 NOT FOUND</>} />
-          </Routes>
-        </BrowserRouter>
-      </AuthWrapper>
-    </RecoilRoot>
+    <>
+      <ToastContainer
+        position='top-right'
+        autoClose={3000}
+        hideProgressBar={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <RecoilRoot>
+        <AuthWrapper>
+          <Modal />
+          <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<Landing />} />
+              <Route path='/signup' element={<SignUp />} />
+              <Route path='/funding' element={<Funding />} />
+              <Route path='/detail/:id' element={<Detail />} />
+              <Route path='/mypage' element={<MyPage />} />
+              <Route path='/auth/google' element={<Auth />} />
+              <Route path='*' element={<>404 NOT FOUND</>} />
+            </Routes>
+          </BrowserRouter>
+        </AuthWrapper>
+      </RecoilRoot>
+    </>
   );
 };
 
--- src/components/Registration/index.tsx
+++ src/components/Registration/index.tsx
@@ -1,30 +1,63 @@
+import React, { useState } from 'react';
 import CloseIcon from 'assets/svgs/Close';
 import * as S from './style';
-import { useState } from 'react';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 interface GenerateModalProps {
   closeModal: () => void;
 }
 
+interface RegistrationState {
+  projectName: string;
+  projectDescription: string;
+  thumbnail: string;
+  additionalImage: string;
+  fundingGoal: number;
+}
+
 const Registration = ({ closeModal }: GenerateModalProps) => {
   const [tab, setTab] = useState<boolean>(true);
-  const [selectedThumbnale, setselectedThumbnale] = useState<string>('');
-  const [selectedImage, setselectedImage] = useState<string>('');
+  const [registrationData, setRegistrationData] = useState<RegistrationState>({
+    projectName: '',
+    projectDescription: '',
+    thumbnail: '',
+    additionalImage: '',
+    fundingGoal: 1000,
+  });
 
-  const handleThumbnaleChange = (
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    field: keyof RegistrationState,
+  ) => {
+    setRegistrationData({ ...registrationData, [field]: event.target.value });
+  };
+
+  const handleFileChange = (
     event: React.ChangeEvent<HTMLInputElement>,
+    field: keyof RegistrationState,
   ) => {
     const file = event.target.files?.[0];
     if (file) {
-      setselectedThumbnale(URL.createObjectURL(file));
+      const fileUrl = URL.createObjectURL(file);
+      setRegistrationData({ ...registrationData, [field]: fileUrl });
     }
   };
 
-  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    if (file) {
-      setselectedImage(URL.createObjectURL(file));
+  const handleRegistration = () => {
+    const isEmpty = Object.values(registrationData).some((value) => !value);
+    if (isEmpty) {
+      toast.warn('모든 필드를 채워주세요.', {
+        position: toast.POSITION.TOP_CENTER,
+      });
+      return;
     }
+
+    console.log('Registration Data:', registrationData);
+    closeModal();
+    toast.success('프로젝트가 성공적으로 등록되었습니다.', {
+      position: toast.POSITION.TOP_CENTER,
+    });
   };
 
   return (
@@ -37,9 +70,17 @@ const Registration = ({ closeModal }: GenerateModalProps) => {
       {tab ? (
         <S.ModalContents>
           <S.Body>프로젝트 이름</S.Body>
-          <S.Input placeholder='이름 입력해주세요.' />
+          <S.Input
+            placeholder='이름 입력해주세요.'
+            value={registrationData.projectName}
+            onChange={(e) => handleInputChange(e, 'projectName')}
+          />
           <S.Body>프로젝트 설명</S.Body>
-          <S.TextArea placeholder='프로젝트에 대한 설명을 입력해주세요.' />
+          <S.TextArea
+            placeholder='프로젝트에 대한 설명을 입력해주세요.'
+            value={registrationData.projectDescription}
+            onChange={(e) => handleInputChange(e, 'projectDescription')}
+          />
           <S.Wrapper>
             <div />
             <div />
@@ -58,14 +99,16 @@ const Registration = ({ closeModal }: GenerateModalProps) => {
                   프로젝트 썸네일을 업로드해주세요.
                 </S.Body>
                 <S.ImageLabel htmlFor='file1'>
-                  {<S.InputImage src={selectedThumbnale} /> || (
+                  {registrationData.thumbnail ? (
+                    <S.InputImage src={registrationData.thumbnail} />
+                  ) : (
                     <h1>파일 선택하기</h1>
                   )}
                 </S.ImageLabel>
                 <input
                   type='file'
                   id='file1'
-                  onChange={handleThumbnaleChange}
+                  onChange={(e) => handleFileChange(e, 'thumbnail')}
                   style={{ display: 'none' }}
                 />
               </S.UploadImage>
@@ -77,23 +120,36 @@ const Registration = ({ closeModal }: GenerateModalProps) => {
                   추가로 이미지를 업로드해주세요.
                 </S.Body>
                 <S.ImageLabel htmlFor='file2'>
-                  {<S.InputImage src={selectedImage} /> || '파일 선택하기'}
+                  {registrationData.additionalImage ? (
+                    <S.InputImage src={registrationData.additionalImage} />
+                  ) : (
+                    '파일 선택하기'
+                  )}
                 </S.ImageLabel>
                 <input
                   type='file'
                   id='file2'
-                  onChange={handleImageChange}
+                  onChange={(e) => handleFileChange(e, 'additionalImage')}
                   style={{ display: 'none' }}
                 />
               </S.UploadImage>
             </div>
           </S.Flex>
+          <S.Body style={{ marginTop: '30px' }}>목표 펀딩 금액</S.Body>
+          <S.Input
+            type='number'
+            min='1000'
+            placeholder='목표 펀딩 금액을 입력해주세요. (최소 1,000원)'
+            value={registrationData.fundingGoal}
+            onChange={(e) => handleInputChange(e, 'fundingGoal')}
+            style={{ marginBottom: '100px' }}
+          />
           <S.Wrapper>
             <S.Button onClick={() => setTab((prev) => !prev)}>
               이전으로
             </S.Button>
             <div />
-            <S.Button>등록하기</S.Button>
+            <S.Button onClick={handleRegistration}>등록하기</S.Button>
           </S.Wrapper>
         </S.ModalContents>
       )}
